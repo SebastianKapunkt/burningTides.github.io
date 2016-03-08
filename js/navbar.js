6faec1ba30961e99f47c7b10cea4c4450aaf5ac8
Sebastian Kapunkt <kindt.sebastian@outlook.com>
2016-03-08 12:29:08 +0100

    complete rework
    initial commit for new page

---
+++ js/navbar.js
@@ -0,0 +1,19 @@
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar-container").offset().top > 150) {
+        $(".navbar-container").removeClass("nav-scrolled-top");
+    } else{
+        $(".navbar-container").addClass("nav-scrolled-top");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
