8cec981a15605a2cede7c8dcfb18b0124d5723df
Sebastian Kapunkt <kindt.sebastian@outlook.com>
2016-03-17 17:58:35 +0100

    new Slider
    * integrated new slider
    * started work at responsebility

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Devbook on 3/17/16.
+ */
+jQuery(document).ready(function($) {
+    $('#banner-fade').bjqs({
+        'height' : 520,
+        'width' : 1000,
+        'responsive' : true,
+        nexttext : 'Prev',
+        prevtext : 'Next'
+    });
+});
