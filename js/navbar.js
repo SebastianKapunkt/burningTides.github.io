7aa65a0f49a4426eacf7ef1646b71acd6a75b890
Sebastian Kapunkt <kindt.sebastian@outlook.com>
2016-03-20 16:30:26 +0100

    navbar burger
    at small resolution a burger will show up, to collapse navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,19 +1,35 @@
+var isClicked = false;
+
 //jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
+$(window).scroll(function () {
     if ($(".navbar-container").offset().top > 150) {
         $(".navbar-container").removeClass("nav-scrolled-top");
-    } else{
+    } else {
         $(".navbar-container").addClass("nav-scrolled-top");
     }
 });
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
+$(function () {
+    $('a.page-scroll').bind('click', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
+        $(".fel-navbar").removeClass("collapsed-nav");
+        isClicked = false;
+    });
+});
+
+$(document).ready(function () {
+    $('#burger').click(function (e) {
+        if (!isClicked) {
+            $(".fel-navbar").addClass("collapsed-nav");
+            isClicked = true;
+        } else {
+            $(".fel-navbar").removeClass("collapsed-nav");
+            isClicked = false;
+        }
     });
 });
