094e4546f9823a8c6bafe62d8d856b8293c6bd56
Sebastian Kapunkt <kindt.sebastian@outlook.com>
2016-03-25 15:44:22 +0100

    again new design
    initial commit for new design

---
+++ js-next/navbar.js
@@ -0,0 +1,39 @@
+var isClicked = false;
+
+// show other navbar at scrolled top state
+$(window).scroll(function () {
+    if ($(".navbar-container").offset().top > 150) {
+        $(".navbar-container").removeClass("nav-scrolled-top");
+    } else {
+        $(".navbar-container").addClass("nav-scrolled-top");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function () {
+    $('a.page-scroll').bind('click', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutExpo');
+        // also close burger menu
+        event.preventDefault();
+        $(".fel-navbar").removeClass("collapsed-nav");
+        isClicked = false;
+    });
+});
+
+// burger collapse, by adding and removing css class
+$(document).ready(function () {
+    $('#burger').click(function (e) {
+        if (!isClicked) {
+            $(".fel-navbar").addClass("collapsed-nav");
+            isClicked = true;
+        } else {
+            $(".fel-navbar").removeClass("collapsed-nav");
+            isClicked = false;
+        }
+    });
+});
+
+
---
+++ js-next/slider.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function($) {
+    $('#banner-fade').bjqs({
+        'height' : 550,
+        'width' : 800,
+        'responsive' : true,
+    });
+});
