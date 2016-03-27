420304e30eff2468185c4208d6521543eb53f37c
Sebastian Kapunkt <kindt.sebastian@outlook.com>
2016-03-27 13:11:31 +0200

    reformat code

--- js-next/slider.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($) {
-    $('#banner-fade').bjqs({
-        'height' : 550,
-        'width' : 800,
-        'responsive' : true,
-    });
-});
--- js/slider.js
+++ js/slider.js
@@ -1,10 +1,10 @@
 /**
  * Created by Devbook on 3/17/16.
  */
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
     $('#banner-fade').bjqs({
-        'height' : 550,
-        'width' : 600,
-        'responsive' : true,
+        'height': 550,
+        'width': 600,
+        'responsive': true,
     });
 });
