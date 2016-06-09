b2b1f6b9aee8162b01c5312379c54e3269d29844
Sebastian Kapunkt <kindt.sebastian@outlook.com>
2016-06-10 00:43:56 +0200

    feature section got own styles

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,8 @@
 var name_space = angular.module('burning_tides_app', []);
 
-name_space.controller("TestFunction", ['$scope', '$http', function ($scope, $http) {
+name_space.controller("NewsController", ['$scope', '$http', function ($scope, $http) {
     $http.get('app/json/data.json').success(function (data) {
-        $scope.test_var = data;
+        $scope.news = data;
     });
     $scope.setCurrentNews = function (item){
         $scope.news_content = item.content;
